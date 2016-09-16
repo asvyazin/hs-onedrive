07afcbdc2d99081a8672c63aee21f3f900772f0a
Alexander Svyazin <guybrush@live.ru>
2016-09-16 15:34:39 +0500

    FileFacet: field ""mimeType" is optional

--- src/Onedrive/Types/FileFacet.hs
+++ src/Onedrive/Types/FileFacet.hs
@@ -1,26 +1,27 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Onedrive.Types.FileFacet where
 
 
 import Control.Lens (makeLensesWith, camelCaseFields)
 import Onedrive.Types.HashesType (HashesType)
-import Data.Aeson (FromJSON(parseJSON), Value(Object), (.:))
+import Data.Aeson (FromJSON(parseJSON), Value(Object), (.:), (.:?))
 import Data.Text (Text)
 
 
 data FileFacet =
   FileFacet
-  { fileFacetMimeType :: Text
+  { fileFacetMimeType :: Maybe Text
   , fileFacetHashes :: HashesType
   } deriving (Show)
 
 
 instance FromJSON FileFacet where
   parseJSON (Object o) =
-    FileFacet <$> o .: "mimeType" <*> o .: "hashes"
+    FileFacet <$> o .:? "mimeType" <*> o .: "hashes"
   parseJSON _ =
     error "Invalid FileFacet JSON"
 
