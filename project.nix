d19976eaf42929cc284abb943448db9fcae9e0cc
Alexander Svyazin <guybrush@live.ru>
2017-08-17 22:54:12 +0500

    extract project.nix from shell.nix

---
+++ project.nix
@@ -0,0 +1,32 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, base, bytestring, conduit
+      , conduit-combinators, exceptions, http-client, http-conduit
+      , http-types, lens, mtl, stdenv, stm, text, transformers
+      }:
+      mkDerivation {
+        pname = "hs-onedrive";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [
+          aeson base bytestring conduit conduit-combinators exceptions
+          http-client http-conduit http-types lens mtl stm text transformers
+        ];
+        testHaskellDepends = [ base ];
+        homepage = "https://github.com/asvyazin/hs-onedrive#readme";
+        description = "Haskell bindings to the Onedrive API";
+        license = stdenv.lib.licenses.bsd3;
+        doHaddock = false;
+        doCheck = false;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+in
+  haskellPackages.callPackage f {}
